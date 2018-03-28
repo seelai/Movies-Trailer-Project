61cef8bc42768b4381c7efe38dc9bec973d9a8a6
seelai <seelai92@gmail.com>
2018-03-28 21:14:12 +0800

    Correct code to PEP8 requirements
    Correct code to PEP8 requirements

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+
 class Movie():
     """ This class provides a way to store movie related information
 
@@ -10,15 +11,15 @@ class Movie():
             trailer_youtube_url - URL to youtube video showing the trailer
 
         Functions:
-            show_trailer - open the trailer in new window using given url 
+            show_trailer - open the trailer in new window using given url
     """
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
-        self.poster_image_url =  poster_image
+        self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
